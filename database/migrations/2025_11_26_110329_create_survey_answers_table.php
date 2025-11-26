1883c0ef09e794d80bce855022e81c5372f6d3a9
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-11-26 14:07:25 +0300

    feat: added survey answers model and migration

---
+++ database/migrations/2025_11_26_110329_create_survey_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('survey_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('survey_response_id')->constrained('survey_responses')->onDelete('cascade');
+            $table->foreignId('survey_question_id')->constrained('survey_questions')->onDelete('cascade');
+            $table->text('answer')->nullable(); // store text answer or option ids (comma separated / JSON)
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('survey_answers');
+    }
+};
