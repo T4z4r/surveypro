a11fa3c279053411a6db34b2f7c65f812e7573ab
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-11-26 13:51:30 +0300

    feat: added survey model and migration

---
+++ app/Models/Survey.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    protected $fillable = ['title','description','status'];
+
+    public function questions() {
+        return $this->hasMany(SurveyQuestion::class);
+    }
+
+    public function responses() {
+        return $this->hasMany(SurveyResponse::class);
+    }
+}
